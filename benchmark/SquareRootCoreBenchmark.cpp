286b49fe52f6ec3f0593f128f7b02c8ec6f4bd6e
Christopher Wecht <christopher.wecht@student.kit.edu>
2017-10-23 12:25:41 +0200

    benchmarks are working now with gcc

--- benchmark/SquareRootCoreBenchmark.cpp
+++ benchmark/SquareRootCoreBenchmark.cpp
@@ -25,7 +25,11 @@ using MyStateVector = UKF::StateVector<
     UKF::Field<AngularVelocity, UKF::Vector<3>>
 >;
 
-template <> constexpr real_t UKF::Parameters::AlphaSquared<MyStateVector> = 1e-6;
+namespace UKF {
+namespace Parameters {
+template <> constexpr real_t AlphaSquared<MyStateVector> = 1e-6;
+}
+
 
 /*
 State vector process model. One version takes body frame kinematic
@@ -60,6 +64,8 @@ MyStateVector MyStateVector::derivative<>() const {
     return derivative(UKF::Vector<3>(0, 0, 0), UKF::Vector<3>(0, 0, 0));
 }
 
+}
+
 /*
 Set up measurement vector class. The order of these is changed to prevent
 linker collisions with the ones in CoreBenchmark.cpp.
@@ -86,6 +92,7 @@ using MyCore = UKF::SquareRootCore<
     UKF::IntegratorRK4
 >;
 
+namespace UKF {
 /*
 Define measurement model to be used in tests. NOTE: These are just for
 testing, don't expect them to make any physical sense whatsoever.
@@ -124,6 +131,8 @@ UKF::Vector<3> MyMeasurementVector::expected_measurement
 template <>
 MyMeasurementVector::CovarianceVector MyMeasurementVector::measurement_root_covariance =
     MyMeasurementVector::CovarianceVector();
+
+}
 
 /*
 Initialise covariances as square roots to be comparable with CoreBenchmark.cpp.
